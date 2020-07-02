8c0ff9385665bf43c8b0e0c5df4dc5d2d9b4edd0
SincYW <y-watanabe@sinc-part.co.jp>
2020-07-02 16:37:32 +0900

    add malloc sample

--- c/sample_thread/main.c
+++ c/sample_thread/main.c
@@ -2,29 +2,29 @@
 #include <pthread.h>
 #include <unistd.h>
 
-void* boil_udon(void* pu){
+void* boil_udon(){
     int second = 2;
     printf("うどんを茹でます\n");
     sleep(second);
     printf("うどんが茹で上がりました\n");
 }
 
-void* make_tuyu(void* p){
+void* make_tuyu(){
     printf("ツユをつくります\n");
     printf("ツユができました\n");
 }
 
 int main()
 {
-    /ptthread_t...スレッドを格納する型/
+    /*ptthread_t...スレッドを格納する型*/
     pthread_t thread1, thread2;
-    /第一引数:スレッド変数へのポインタ
-    　第二引数:スレッドの属性へのポインタ/
-    pthread_create(&thread1, NULL, boil_udon, NULL);
-    pthread_create(&thread2, NULL, make_tuyu, NULL);
+    /*第一引数:スレッド変数へのポインタ
+    　第二引数:スレッドの属性へのポインタ*/
+    pthread_create(&thread1, (void*)0, boil_udon, (void*)0);
+    pthread_create(&thread2, (void*)0, make_tuyu, (void*)0);
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    pthread_join(thread1, (void*)0);
+    pthread_join(thread2, (void*)0);
 
     printf("盛り付けます\n");
     printf("うどんができました！\n");
